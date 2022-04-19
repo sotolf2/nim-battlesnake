3c5b64d1921007d3127499879989e8490461f58d
sotolf2 <56679258+sotolf2@users.noreply.github.com>
2022-04-19 11:02:34 +0000

    Starting battlesnake

--- main.nim
+++ main.nim
@@ -1,8 +1,7 @@
-import htmlgen
 import jester
 import jsony
 
-type BattleSnake = object
+type Customizations = object
   apiversion: string
   author: string
   color: string
@@ -10,15 +9,96 @@ type BattleSnake = object
   tail: string
   version: string
 
-proc snakeProperties(): BattleSnake =
+type RoyaleSettings = object
+  shrinkEveryNTurns: int
+
+type SquadSettings = object
+  allowBodyCollisions: bool
+  sharedElimination: bool
+  sharedHealth: bool
+  sharedLength: bool
+  
+type RulesetSettings = object
+  foodSpawnChance: int
+  minimumFood: int
+  hazardDamagePerTurn: int
+  hazardMap: string
+  hazardMapAuthor: string
+  royale: RoyaleSettings
+  squad: SquadSettings
+  
+type Ruleset = object
+  name: string
+  version: string
+  settings: RulesetSettings
+  
+type GameObject = object
+  id: string
+  ruleset: Ruleset
+  timeout: int
+  source: string
+
+type Coord = object
+  x: int
+  y: int
+
+type BattleSnake = object
+  id: string
+  name: string
+  health: int
+  body: seq[Coord]
+  latency: string
+  head: Coord
+  length: int
+  shout: string
+  squad: string
+  customizations: Customizations
+  
+type Board = object
+  height: int
+  width: int
+  food: seq[Coord]
+  hazards: seq[Coord]
+  snakes: seq[BattleSnake]
+  
+type Game = object
+  game: GameObject
+  turn: int
+  board: Board
+  you: BattleSnake
+
+type Move = object
+  move: string
+  shout: string
+
+proc snakeProperties(): Customizations =
   result.apiversion = "1"
   result.author = "sotolf"
   result.color = "#cccccc"
   result.head = "default"
   result.tail = "default"
   result.version = "0.0.1 beta"
-  
+
+proc makeMove(): Move =
+  result.move = "Up"
+  result.shout = "Going Up"
+
+var game: Game 
+
 routes:
   get "/":
     let snakeProperties = snakeProperties()
     resp(Http200, toJson(snakeProperties))
+
+  get "/start":
+    game = request.body.fromJson(Game)
+    resp(Http200, request.body)
+
+  get "/move":
+    game = request.body.fromJson(Game)
+    var move = makeMove()
+    resp(Http200, toJson(move))
+
+  get "/end":
+    game = request.body.fromJson(Game)
+    resp(Http200, "Ended")
